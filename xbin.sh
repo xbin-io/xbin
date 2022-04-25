39795863ad5adcdc7d8c4c97a86c4c911aa3543c
laixintao <laixintaoo@gmail.com>
2022-04-25 11:03:42 +0800

    bugfix: fix bash quote
    credit: https://twitter.com/Mingkwind

--- xbin.sh
+++ xbin.sh
@@ -2,7 +2,7 @@ function xbin() {
   command="$1"
   args="${@:2}"
   if [ -t 0 ]; then
-    curl -X "POST https://xbin.io/${command}" -H "X-Args: ${args}"
+    curl -X POST "https://xbin.io/${command}" -H "X-Args: ${args}"
   else
     curl --data-binary @- "https://xbin.io/${command}" -H "X-Args: ${args}"
   fi
