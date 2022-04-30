ef78dd23eccde4c2868bddb16b87dfd740c2ae5d
laixintao <laixintaoo@gmail.com>
2022-04-30 17:32:51 +0800

    make curl slient.

--- xbin.sh
+++ xbin.sh
@@ -2,8 +2,8 @@ function xbin() {
   command="$1"
   args="${@:2}"
   if [ -t 0 ]; then
-    curl -X POST "https://xbin.io/${command}" -H "X-Args: ${args}"
+    curl -sS -X POST "https://xbin.io/${command}" -H "X-Args: ${args}"
   else
-    curl --data-binary @- "https://xbin.io/${command}" -H "X-Args: ${args}"
+    curl -sS --data-binary @- "https://xbin.io/${command}" -H "X-Args: ${args}"
   fi
 }
